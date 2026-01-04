a96d1cb6a911e57ba6d723f834c81224fee082f4
Saurabh <saurabhpatil.sp536@gmail.com>
2026-01-04 17:43:12 +0530

    fix commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {  ShopContextProvider } from './Components/Context/ShopContext.jsx';
+import {  ShopContextProvider } from './components/Context/ShopContext.jsx';
 
 
 import { ToastContainer } from "react-toastify";
