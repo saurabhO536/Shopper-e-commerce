c0567dfcbc9a6d374d47eec11771b131533af387
DIPAKK2310 <dipakkhare217@gmail.com>
2026-01-13 18:01:17 +0000

    feat:add footer & navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
 import './App.css'
-import Navbar from './components/Navbar/Navbar'
+import Navbar from './components/Navbar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Shop from './components/Pages/Shop';
 import Product from './components/Pages/Product';
 import ShopCategory from './components/Pages/ShopCategory';
 import LoginSignUp from './components/Pages/LoginSignUp';
 import Cart from './components/Pages/Cart';
-import Footer from './components/Footer/Footer';
-import banner_mens from './assets/banner_mens.png'
-import banner_women from './assets/banner_women.png'
-import banner_kids from './assets/banner_kids.png'
-import PageWrapper from './components/PageWrapper/PageWrapper';
+import Footer from './components/Footer';
+import banner_mens from '../src/assets/banner_mens.png'
+import banner_women from '../src/assets/banner_women.png'
+import banner_kids from '../src/assets/banner_kids.png'
+import PageWrapper from './components/PageWrapper';
 import SignUp from './components/Pages/SignUp';
 
 function App() {
