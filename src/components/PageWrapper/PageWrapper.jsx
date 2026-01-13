f7ffec3c0b219e12ad8028b5288585800c68f443
DIPAKK2310 <dipakkhare217@gmail.com>
2026-01-13 18:02:08 +0000

    Refractor & delete

--- src/components/PageWrapper/PageWrapper.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../PageWrapper/PageWrapper.css";
-
-const PageWrapper = ({ children }) => {
-  return <div className="page-animation">{children}</div>;
-};
-
-export default PageWrapper;
