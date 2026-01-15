b28704e72c8c3fb65dc37d8fb22e9622ce0a879c
DIPAKK2310 <dipakkhare217@gmail.com>
2026-01-15 17:55:54 +0000

    feat:imports change

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Shop from './components/Pages/Shop';
-import Product from './components/Pages/Product';
-import ShopCategory from './components/Pages/ShopCategory';
-import LoginSignUp from './components/Pages/LoginSignUp';
-import Cart from './components/Pages/Cart';
+import Shop from './Pages/Shop';
+import Product from './Pages/Product';
+import ShopCategory from './Pages/ShopCategory';
+import LoginSignUp from './Pages/LoginSignUp';
+import Cart from './Pages/Cart';
 import Footer from './components/Footer';
 import banner_mens from '../src/assets/banner_mens.png'
 import banner_women from '../src/assets/banner_women.png'
 import banner_kids from '../src/assets/banner_kids.png'
 import PageWrapper from './components/PageWrapper';
-import SignUp from './components/Pages/SignUp';
+import SignUp from './Pages/SignUp';
 
 function App() {
 
